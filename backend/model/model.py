840f6abf736cfbcf63d60adcf71907e9bfc698c8
Pasan Kalhara <kalharap84@gmail.com>
2026-05-17 14:10:47 +0530

    Refactor model.py to ensure model directory is created if it doesn't exist

--- backend/model/model.py
+++ backend/model/model.py
@@ -8,3 +8,7 @@ import numpy as np
 import yfinance as yf
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import load_model
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+MODEL_DIR = os.path.join(BASE_DIR, "model")
+os.makedirs(MODEL_DIR, exist_ok=True)
