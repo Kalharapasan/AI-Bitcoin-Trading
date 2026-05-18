b944ba779c581df2166c7e7663193bb9f171455f
Pasan Kalhara <kalharap84@gmail.com>
2026-05-18 23:25:34 +0530

    Add scale_features function to scale data using the provided scaler

--- backend/model/scaler.py
+++ backend/model/scaler.py
@@ -4,3 +4,4 @@ from sklearn.preprocessing import MinMaxScaler
 def create_scaler(feature_range=(0, 1)):
     return MinMaxScaler(feature_range=feature_range)
 
+def scale_features(scaler, data, fit=True):
